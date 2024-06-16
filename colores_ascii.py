ac6353bc8285f9a73561d2a2da32734bd2d70968
Bolkof <142789552+Bolkof@users.noreply.github.com>
2024-06-16 06:32:00 -0500

    Agregar el if name igual main a  colores_ascii.py

--- colores_ascii.py
+++ colores_ascii.py
@@ -1,3 +1,4 @@
+# Definición de códigos de colores y estilos
 rojo = "\033[91m"
 cyan = "\033[96m"
 magenta = "\033[95m"
@@ -13,7 +14,6 @@ parpadeante = "\033[5m"
 invertido = "\033[7m"
 tachado = "\033[9m"
 
-
 amarillo_brillante = "\033[93;1m"
 rojo_brillante = "\033[91;1m"
 verde_brillante = "\033[92;1m"
@@ -21,3 +21,12 @@ azul_brillante = "\033[94;1m"
 magenta_brillante = "\033[95;1m"
 cyan_brillante = "\033[96;1m"
 blanco_brillante = "\033[97;1m"
+
+# Ejemplo de uso
+if __name__ == "__main__":
+	print(rojo + "Texto en rojo" + reset_color)
+	print(verde + "texto verde" + reset_color)
+	print(verde_brillante + "Texto en verde brillante" + reset_color)
+	print(azul + subrayado + "Texto en azul subrayado" + reset_color)
+	print(magenta_brillante + parpadeante + "Texto en magenta brillante y parpadeante" + reset_color)
+	print(cyan + invertido + "Texto en cyan con fondo invertido" + reset_color)
